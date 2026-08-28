363619d54c79bcd20846269939d30d96d199d4d3
beatysunshine7-afk <beatysunshine7@gmail.com>
2026-08-28 10:15:07 +0700

    Hoan thanh bai thuc hanh Buoi 2

---
+++ Hoat_dong_1_3.py
@@ -0,0 +1,13 @@
+ho_ten = input("Nhap ho ten: ")
+nam_sinh = int(input("Nhap nam sinh: "))
+diem_tb = float(input("Nhap diem trung binh: "))
+
+print(f"Ho ten: {ho_ten} - Nam sinh: {nam_sinh} - DTB: {diem_tb:.2f}")
+
+print("Ho ten: {} - Nam sinh: {} - DTB: {:.2f}".format(
+    ho_ten, nam_sinh, diem_tb
+))
+
+print("Ho ten: %s - Nam sinh: %d - DTB: %.2f" % (
+    ho_ten, nam_sinh, diem_tb
+))
---
+++ Hoat_dong_4_4.py
@@ -0,0 +1,5 @@
+ho_ten_tho = "   nguyen    van   an   "
+
+ho_ten_sach = " ".join(ho_ten_tho.split()).title()
+
+print(ho_ten_sach)
